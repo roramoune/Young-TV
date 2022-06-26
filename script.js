12b333c85558e295e9f3758f2be58f146b95cd26
Ramoune O. Roberts <71909892+shikarinmodotoku@users.noreply.github.com>
2022-06-25 19:38:58 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,130 @@
+function doletter(){
+    let letter = document.getElementById('letter').value;
+    if(letter === "A"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/eEmXHJX7Wb0';
+        }
+    else if(letter === "B"){
+        var iframe = document.getElementById('iframe');
+        iframe.src = 'https://www.youtube-nocookie.com/embed/0pBt0F2AN7E';
+        }
+    else if(letter === "C"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/_F_fH625We4';
+        }
+    else if(letter === "D"){
+        var iframe = document.getElementById('iframe');
+        iframe.src = 'https://www.youtube-nocookie.com/embed/HnpdQsrhGp4';
+        }
+    else if(letter === "E"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/DxyzoMkz24I';
+        }
+    else if(letter === "F"){
+        var iframe = document.getElementById('iframe');
+        iframe.src = 'https://www.youtube-nocookie.com/embed/-kZUq9u7SUk';
+        }
+    else if(letter === "G"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/OxF-aEqNpLU';
+        }
+    else if(letter === "H"){
+        var iframe = document.getElementById('iframe');
+        iframe.src = 'https://www.youtube-nocookie.com/embed/ESrsVGqucxc';
+    }
+    else if(letter === "I"){
+        var iframe = document.getElementById('iframe');
+        iframe.src = 'https://www.youtube-nocookie.com/embed/0Ug-7Ff9GV4';
+        }
+    else if(letter === "J"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/GC9I6Vxj5yM';
+        }
+    else if(letter === "K"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/TxH4HwSdu9c';
+        }
+    else if(letter === "L"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/aw8_argqbmA';
+        }
+    else if(letter === "M"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/dxrS93gOdck';
+        }
+    else if(letter === "N"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/l2qfGLacGGw';
+        }
+    else if(letter === "O"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/BM_o8de1UBM';
+        }
+    else if(letter === "P"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/eWgkwHlqrTM';
+        }    
+    else if(letter === "Q"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/zNW2XbwypXA';
+        }               
+    else if(letter === "R"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/aC_0vQRQe-s';
+        }
+    else if(letter === "S"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/3Gb20Cc_4-A';
+        }
+    else if(letter === "T"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/wLypPV-n_0Y';
+        }     
+    else if(letter === "U"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/qET_Pk2yXXo';
+        }
+    else if(letter === "V"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/KAYe_TCJXlg';
+        }
+    else if(letter === "W"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/jxBDGPvZPi0';
+        }
+    else if(letter === "X"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/O_rEI2uc7zw';
+        }
+    else if(letter === "Y"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/lURkIT4Wu8g';
+        }
+    else if(letter === "Z"){
+    var iframe = document.getElementById('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/Y2i9i5pb--M';
+        }
+}
+
+
+function scrollgame(){
+        games.scrollTo(0,0);
+    }
+    function scrollgame1(){
+        games.scrollTo(1255,0);
+    }
+    function scrollgame2(){
+        games.scrollTo(2511,0);
+    }
+    function scrollgame3(){
+        games.scrollTo(3769,0);
+    }
+    function scrollgame4(){
+        games.scrollTo(5026,0);
+    }
+    function scrollgame5(){
+        games.scrollTo(6277,0);
+    }
+    function scrollgame6(){
+        games.scrollTo(7540,0);
+    }
